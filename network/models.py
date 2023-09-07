40c21be430e416af263213ae5ca9a658efcd4458
FakirHerif <basdemir35@hotmail.com.tr>
2023-09-07 18:27:12 +0300

    added like class and applied migrations

--- network/models.py
+++ network/models.py
@@ -19,3 +19,10 @@ class Follow(models.Model):
 
     def __str__(self):
         return f"{self.user} is following {self.user_followed}"
+    
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="like_user")
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="like_post")
+
+    def __str__(self):
+        return f"{self.user} liked {self.post}"
